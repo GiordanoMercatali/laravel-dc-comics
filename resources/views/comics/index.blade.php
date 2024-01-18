e3166b149044b0107c316f955f9e17d458381f49
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-18 18:55:11 +0100

    add: crud index file

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <h1>La lista delle paste</h1>
+        <div class="text-end">
+          <a class="btn btn-primary" href="{{ route('comics.create') }}">Add comic</a>
+        </div>
+
+        <ul>
+            @foreach ($comics as $comic)
+            <li>
+                <span>{{$comic->id}}</span>
+                <span>{{$comic->title}}</span>
+                <span>{{$comic->series}}</span>
+                <span>{{$comic->price}}</span>
+                <a class="btn btn-success" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Details</a>
+            </li>
+            @endforeach
+        </ul>
+        
+    </div>
+@endsection
