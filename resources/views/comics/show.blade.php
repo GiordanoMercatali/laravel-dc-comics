14708dd8977869a97aaac1bdc5a976ad996f67b5
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-18 19:00:22 +0100

    add: crud show file

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>{{ $comic->title }}</h1>
+        <img class="w-25" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+
+        <hr>
+        <p>{{ $comic->description }}</p>
+
+        <ul>
+            <li>
+                Series: {{ $comic->series }}
+            </li>
+            <li>
+               Price: {{ $comic->price }}
+            </li>
+            <li>
+                Sale Date: {{ $comic->sale_date }}
+            </li>
+            <li>
+                Type: {{ $comic->type }}
+            </li>
+        </ul>
+    </div>    
+@endsection
