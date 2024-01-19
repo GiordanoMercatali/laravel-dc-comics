56b67029912b0d2e12d6e6ea6473e3f2438bbe65
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-19 15:04:54 +0100

    updated migrations

--- database/migrations/2024_01_18_144920_update_comics_table.php
+++ database/migrations/2024_01_18_144920_update_comics_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('comics', function(Blueprint $table){
             $table->id();
             $table->string('title');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->string('thumb');
             $table->string('price');
             $table->string('series');
