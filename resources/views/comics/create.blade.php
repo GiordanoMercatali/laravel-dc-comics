509f5a3a9421c3ae7c446fd6da08b94180e02f54
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-22 19:38:17 +0100

    add: old to create to retain changes

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -20,37 +20,37 @@
                     
                     <div class="m-2">
                         <label for="title" class="form-label">Title</label>
-                        <input required type="text" class="form-control" id="title" name="title">
+                        <input required type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
                     </div>
 
                     <div class="m-2">
                         <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                        <textarea class="form-control" id="description" rows="3" name="description">{{old('description')}}</textarea>
                     </div>
 
                     <div class="m-2">
                         <label for="thumb" class="form-label">Thumb</label>
-                        <input type="text" class="form-control" id="thumb" name="thumb">
+                        <input type="text" class="form-control" id="thumb" name="thumb" value="{{old('thumb')}}">
                     </div>
 
                     <div class="m-2">
                         <label for="series" class="form-label">Series</label>
-                        <input type="text" class="form-control" id="series" name="series">
+                        <input type="text" class="form-control" id="series" name="series" value="{{old('series')}}">
                     </div>
 
                     <div class="m-2">
                         <label for="sale_date" class="form-label">Sale Date</label>
-                        <input type="date" class="form-control" id="sale_date" name="sale_date">
+                        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{old('sale_date')}}">
                     </div>
 
                     <div class="m-2">
                         <label for="type" class="form-label">Type</label>
-                        <input type="text" class="form-control" id="type" name="type">
+                        <input type="text" class="form-control" id="type" name="type" value="{{old('type')}}">
                     </div>
 
                     <div class="m-2">
                         <label for="type" class="form-label">Price</label>
-                        <input type="text" class="form-control" id="price" name="price">
+                        <input type="text" class="form-control" id="price" name="price" value="{{old('price')}}">
                     </div>
 
                     <a class="btn btn-danger" href="{{ route('comics.index') }}">Back</a>
